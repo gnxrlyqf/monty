0a006c85939f8c5e003f96c20b8a7e0a2b88c899
hajarmos <hajarbouzaid@gmail.com>
2023-08-26 12:46:29 +0000

    fixxxxxxxxxx

--- 3.c
+++ 3.c
@@ -14,7 +14,7 @@ void mod(stack_t **stack, unsigned int line_number)
 
 	if (!(*stack) || ((*stack)->next && (*stack)->prev))
 	{
-		fprintf(stderr, "L%d: can't div, stack too short", line_number);
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
 		_free();
 		exit(EXIT_FAILURE);
 	}
